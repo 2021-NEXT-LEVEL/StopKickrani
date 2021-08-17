5d1cc2486a6ef45afe73266f5905d29ad6031277
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-08-17 22:46:06 +0900

    MODIFY models

--- backend/main/models.py
+++ backend/main/models.py
@@ -5,17 +5,18 @@ class Video(models.Model):
     url = models.CharField(max_length=1000)
     date = models.DateField()
     location = models.CharField(max_length=100)
-    graph = models.CharField(max_length=100)
 
     class Meta:
-        managed = False
+        #managed = False
         db_table = 'video'
 
 class DetectResult(models.Model):
     rid = models.AutoField(primary_key=True)
     log = models.TimeField()
-    class_field = models.CharField(db_column='class', max_length=45)  # Field renamed because it was a Python reserved word.
+    value = models.CharField(max_length=45)
+    date = models.DateField()
+    location = models.CharField(max_length=100)
 
     class Meta:
-        managed = False
+        #managed = False
         db_table = 'detect_result'
