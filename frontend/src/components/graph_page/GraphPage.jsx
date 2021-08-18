0a0536836f2000a4ddb9a5197fd4c17f8c0f6247
fairyroad <kuj9628@naver.com>
2021-08-19 01:55:54 +0900

    ADD dailygraph

--- frontend/src/components/graph_page/GraphPage.jsx
+++ frontend/src/components/graph_page/GraphPage.jsx
@@ -3,42 +3,41 @@ import styles from './GraphPage.module.css';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { purple } from '@material-ui/core/colors';
-import MyResponsiveLine from "./sections/charts";
-import data from './sections/data.json';
-import { USER_SERVER } from '../../Config'
+import Charts from "./sections/charts";
+import { USER_SERVER } from '../../Config';
 //import data from '../../public/detect_log/data.json';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-  graphDate: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: '10px',
-  },
-});
-
-
-const DrawGraph = () => (
-  <div style={styles}>
-    {/*<h1>Nivo basic demo</h1>*/}
-    <div style={{ height: "450px" }}>
-      <MyResponsiveLine data={data} />
-      {/* <ResponsiveBar data={data} keys={["earnings"]} indexBy="quarter" /> */}
-    </div>
-  </div>
-);
-
 function GraphPage(props) {
+
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+    graphDate: {
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: '10px',
+    },
+  });
+  
+  const DrawGraph = () => (
+    <div style={styles}>
+      {/*<h1>Nivo basic demo</h1>*/}
+      <div style={{ height: "450px" }}>
+        <Charts loc={loc} videoId={videoId}/>
+        {/* <ResponsiveBar data={data} keys={["earnings"]} indexBy="quarter" /> */}
+      </div>
+    </div>
+  );
+  
 
   const classes = useStyles();
 
   const [loc, setLoc] = useState(props.match.params.loc)
   const [videoId, setVideoId] = useState(props.match.params.videoId)
   const [count, setCount] = useState([])
-
-
+  let numOfNonHelmet = 0
+  let numOfOverTwo = 0
   const moveToDetail = () => {
     props.history.push('/detail/' + loc + '/' + videoId)
   }
@@ -61,17 +60,15 @@ function GraphPage(props) {
       .then(response => {
         console.log(response)
 
-        let numOfHelmet = 0
-        let numOfNonHelmet = 0
 
         response.forEach((item) => {
-          if (item.value === 'helmet') {
-            numOfHelmet += 1
-          } else {
+          if (item.value == 'non-helmet') {
             numOfNonHelmet += 1
+          } else if (item.value == 'over-two') {
+            numOfOverTwo += 1
           }
         })
-        setCount([numOfHelmet, numOfNonHelmet])
+        setCount([numOfOverTwo, numOfNonHelmet])
 
       })
       .catch((err) => {
@@ -94,7 +91,7 @@ function GraphPage(props) {
           DETAIL
         </ColorButton>
       </div>
-      helmet 수 : {count[0]}<p />nonhelmet 수 : {count[1]}
+      non-helmet 수 : {count[1]}<p />over-two 수 : {count[0]}
     </>
   )
 }
--- frontend/src/components/graph_page/sections/charts.jsx
+++ frontend/src/components/graph_page/sections/charts.jsx
@@ -1,69 +1,85 @@
-import React from "react";
-import { ResponsiveLine } from "@nivo/line";
+import React, { useState, useEffect } from "react";
+import { Bar } from "@nivo/bar";
+import { USER_SERVER } from '../../../Config';
 
-const MyResponsiveLine = ({ data }) => (
-  <ResponsiveLine
-    data={data}
-    margin={{ top: 10, right: 70, bottom: 70, left: 60 }}
-    xScale={{ type: "point" }}
-    yScale={{ type: "linear", min:0, stacked: true, min: "auto", max: "auto" }}
-    yFormat=">-.2f"
-    axisTop={null}
-    axisRight={null}
-    axisBottom={{
-      orient: "bottom",
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: "검출 시간",
-      legendOffset: 36,
-      legendPosition: "middle"
-    }}
-    axisLeft={{
-      format: e => Math.floor(e) === e && e, //y축 정수형
-      orient: "left",
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: "검출 횟수",
-      legendOffset: -40,
-      legendPosition: "middle"
-    }}
-    colors={{ scheme: "nivo" }}
-    pointSize={10}
-    pointColor={{ theme: "background" }}
-    pointBorderWidth={2}
-    pointBorderColor={{ from: "serieColor" }}
-    pointLabel="y"
-    pointLabelYOffset={-12}
-    useMesh={true}
-    legends={[
-      {
-        anchor: "bottom",
-        direction: "row",
-        justify: false,
-        translateX: 0,
-        translateY: 70,
-        itemsSpacing: 50,
-        itemDirection: "left-to-right",
-        itemWidth: 80,
-        itemHeight: 20,
-        itemOpacity: 1,
-        symbolSize: 20,
-        symbolShape: "circle",
-        symbolBorderColor: "rgba(0, 0, 0, .5)",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemBackground: "rgba(0, 0, 0, .03)",
-              itemOpacity: 1
-            }
+function Charts(props) {
+
+  const [loc, setLoc] = useState(props.loc)
+  const [videoId, setVideoId] = useState(props.videoId)
+  const [count, setCount] = useState([])
+  let numOfNonHelmet = 0
+  let numOfOverTwo = 0
+
+  const styles = {
+    fontFamily: "sans-serif",
+    fontSize: "14px",
+    textAlign: "center"
+  };
+  useEffect(() => {
+    // get number of (log, value)
+    fetch(`${USER_SERVER}/graph/${loc}/${videoId}`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+
+
+        response.forEach((item) => {
+          if (item.value == 'non-helmet') {
+            numOfNonHelmet += 1
+          } else if (item.value == 'over-two') {
+            numOfOverTwo += 1
           }
-        ]
-      }
-    ]}
-  />
-);
+        })
+        setCount([numOfOverTwo, numOfNonHelmet])
 
-export default MyResponsiveLine;
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+  }, []);
+  
+  const sampleData =
+  [
+    {
+      "id": "non-helmet",
+      "color": "hsl(85, 70%, 50%)",
+      "value": count[1]
+    },
+    {
+      "id": "over-two",
+      "color": "hsl(180, 70%, 50%)",
+      "value":  count[0]
+    }
+  ];
+  
+  const commonProperties = {
+    width: 550,
+    height: 400,
+    data: sampleData,
+    keys: ["value"],
+    indexBy: "id",
+    margin: {
+      top: 10,
+      right: 5,
+      bottom: 60,
+      left: 80
+    }
+  };
+  
+
+  return(
+    <div style={styles}>
+    <Bar
+      {...commonProperties}
+      axisBottom={{
+        // using custom function
+        format: d => `${d}`
+      }}
+    />
+
+  </div>
+  )
+}
+
+export default Charts;
